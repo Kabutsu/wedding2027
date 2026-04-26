a41f8a1da67f7fe1e9b7811d925134161e62d21d
Sam Law <SamJamesLaw@gmail.com>
2026-04-26 22:45:22 +0100

    Some additional config & playing around

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://samandcaro2027.vercel.app/',
+  env: {
+    schema: {
+      PUBLIC_RSVP_LINK: envField.string({ access: 'public', context: 'client' }),
+      PUBLIC_HOTEL_URL: envField.string({ access: 'public', context: 'client' }),
+    }
+  }
+});
---
+++ src/env.d.ts
@@ -0,0 +1,8 @@
+interface ImportMetaEnv {
+  readonly PUBLIC_HOTEL_URL: string;
+  readonly PUBLIC_RSVP_LINK: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
